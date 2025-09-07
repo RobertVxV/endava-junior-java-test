df1b9b4a623f13b5d42b53a81f240790045af177
Robert Vinitchi <robertvinitchi@gmail.com>
2025-09-07 11:23:55 +0300

    expired yesterday logged

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,7 +6,7 @@ INSERT INTO car (id, vin, make, model, year_of_manufacture, owner_id) VALUES (2,
 
 INSERT INTO insurancepolicy (id, car_id, provider, start_date, end_date) VALUES (1, 1, 'Allianz', DATE '2024-01-01', DATE '2024-12-31');
 INSERT INTO insurancepolicy (id, car_id, provider, start_date, end_date) VALUES (2, 1, 'Groupama', DATE '2025-01-01', DATE '2026-01-01');
-INSERT INTO insurancepolicy (id, car_id, provider, start_date, end_date) VALUES (3, 2, 'Allianz', DATE '2025-03-01', DATE '2025-09-30');
+INSERT INTO insurancepolicy (id, car_id, provider, start_date, end_date) VALUES (3, 2, 'Allianz', DATE '2025-03-01', DATE '2025-09-06');
 
 INSERT INTO insuranceclaim (car_id, claim_date, description, amount) VALUES (1, DATE '2024-05-15', 'Minor collision with another vehicle in parking lot. Damage to rear bumper and taillight.', 850.50);
 INSERT INTO insuranceclaim (car_id, claim_date, description, amount) VALUES (1, DATE '2025-08-22', 'Windshield replacement due to stone chip damage while driving on highway.', 325.00);
